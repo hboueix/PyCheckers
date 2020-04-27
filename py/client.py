5708ba6af67657fba8b929c892f8c462e00c906f
Hugo Boueix <hboueix99@gmail.com>
2020-04-27 18:10:25 +0200

    :construction: trying threading

--- py/client.py
+++ py/client.py
@@ -1,8 +1,27 @@
 import socket
+import sys
+import select
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection_server:
+    connection_server.connect(("164.132.225.71", 63000))
+    while True:
+        socket_list = [sys.stdin, connection_server]
+        read_sockets, write_sockets, error_sockets = select.select(sockets_list,[],[])
+        for socks in read_sockets:
+            if socks == connection_server:
+                message = socks.recv(2048)
+                print(message)
+            else:
+                message = sys.stdin.readline()
+                server.send(bytes(message, "utf-8"))
+                sys.stdout.write("<You>") 
+                sys.stdout.write(message) 
+                sys.stdout.flush() 
 
+'''
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection_server:
     connection_server.connect(("164.132.225.71", 63000))
     connection_server.send(bytes("Serveur es-tu là ?", "utf-8"))
     response = connection_server.recv(1024)
     print(response.decode())
+'''
--- server.py
+++ server.py
@@ -1,6 +1,48 @@
 import socket
+import sys
+from _thread import *
 
+def client_thread(connection_client, address_client):
+    connection_client.send("Bienvenue dans la chatroom !")
 
+    while True:
+        try:
+            message = connection_client.recv(2048)
+            if message:
+                print(f'<{address_client[0]}> {message}')
+                message_to_send = "<" + address_client[0] + ">" + message
+                broadcast(message_to_send, connection_client)
+            else:
+                remove(connection_client)
+        except:
+            continue
+
+def broadcast(message, connection_client):
+    for client in list_of_clients:
+        if client != connection:
+            try:
+                client.send(message)
+            except:
+                client.close()
+                remove(client)
+
+def remove(connection_client):
+    if connection_client in list_of_clients:
+        list_of_clients.remove(connection_client)
+
+list_of_clients = []
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(("164.132.225.71", 63000))
+    listen_socket.listen(100)
+    while True:
+        connection_client, address_client = listen_socket.accept()
+        list_of_clients.append(connection_client)
+        print(f'{address_client[0]} connected')
+        start_new_thread(client_thread, (connection_client, address_client))
+
+'''
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
     listen_socket.bind(("", 63000))
     listen_socket.listen()
@@ -8,3 +50,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
     print(address_client)
     message = connection_client.recv(1024)
     connection_client.send(bytes(message.decode() + "\nPrésent !", "utf-8"))
+'''
