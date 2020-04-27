3c51de9257c8e22582669de84c630c47edb1f8a3
Hugo Boueix <hboueix99@gmail.com>
2020-04-27 15:20:39 +0200

    :construction: first try import pygame

---
+++ main.py
@@ -0,0 +1,34 @@
+import subprocess
+import sys
+try:
+    import pygame
+except:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pygame'])
+    import pygame
+
+pygame.init()
+ 
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+ 
+screen = pygame.display.set_mode(size)
+ 
+ball = pygame.image.load("nothing.png")
+ballrect = ball.get_rect()
+ 
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+ 
+    ballrect = ballrect.move(speed) 
+
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
