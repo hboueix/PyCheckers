567c841f9bb1aadf120d7957ee1a49abab0e9683
Hugo Boueix <hboueix99@gmail.com>
2020-05-05 19:04:53 +0200

    :sparkles: chat server works

--- checkers/client.py
+++ checkers/client.py
@@ -1,12 +1,91 @@
+#!/usr/bin/python3
+# -*- coding : utf-8 -*-
+
+import socket
+import select
+import errno
+import sys
+
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 63000
+
+my_username = input("Username : ")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+client_socket.send(username_header + username)
+
+while True:
+    message = input(f"{my_username} > ")
+
+    if message:
+        message = message.encode("utf-8")
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        client_socket.send(message_header + message)
+
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("Connection fermée par le serveur")
+                sys.exit()
+
+            username_length = int(username_header.decode("utf-8"))
+            username = client_socket.recv(username_length).decode("utf-8")
+
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode("utf-8"))
+            message = client_socket.recv(message_length).decode("utf-8")
+
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN or e.errno != errno.EWOULDBLOCK:
+            print('Reading error', str(e))
+            sys.exit()
+        continue
+
+    except Exception as e:
+        print('General error', str(e))
+        sys.exit()
+
+
+'''
+import socket
+
+IP_SERVER = "127.0.0.1"
+PORT = 63000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((IP_SERVER, PORT))
+client.sendall(bytes("Coucou c'est moi le client", "utf-8"))
+while True:
+    in_data = client.recv(2048)
+    print(f"From server : {in_data.decode()}")
+    out_data = input()
+    client.sendall(bytes(out_data, 'utf-8'))
+    if out_data == 'bye':
+        break
+client.close()
+'''
+
+'''
 import socket
 import sys
 import select
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection_server:
-    connection_server.connect(("164.132.225.71", 63000))
+    connection_server.connect(("127.0.0.1", 63000))
     while True:
         sockets_list = [sys.stdin, connection_server]
-        read_sockets, write_sockets, error_sockets = select.select(sockets_list,[],[])
+        read_sockets, write_sockets, error_sockets = select.select(
+            sockets_list, [], [])
         for socks in read_sockets:
             if socks == connection_server:
                 message = socks.recv(2048)
@@ -14,9 +93,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection_server:
             else:
                 message = sys.stdin.readline()
                 connection_server.send(bytes(message, "utf-8"))
-                sys.stdout.write("<You>") 
-                sys.stdout.write(message) 
-                sys.stdout.flush() 
+                sys.stdout.write("<You>")
+                sys.stdout.write(message)
+                sys.stdout.flush()
+'''
 
 '''
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection_server:
--- server.py
+++ server.py
@@ -1,3 +1,128 @@
+#!/usr/bin/python3
+# -*- coding : utf-8 -*-
+
+import socket
+import select
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 63000
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode('utf-8'))
+
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+        return False
+
+
+while True:
+    read_sockets, _, exception_sockets = select.select(
+        sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+
+            user = receive_message(client_socket)
+            if user is False:
+                continue
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print(
+                f"Nouvelle connection depuis {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+
+        else:
+            message = receive_message(notified_socket)
+
+            if message is False:
+                print(
+                    f"Connection terminée avec {clients[notified_socket]['data'].decode('utf-8')}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+
+            user = clients[notified_socket]
+            print(
+                f"Message reçu de {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(
+                        user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
+
+
+'''
+import socket
+import threading
+
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, clientAddress, clientSocket):
+        threading.Thread.__init__(self)
+        self.cSocket = clientSocket
+        print(f'Nouvelle connexion : {clientAddress}')
+
+    def run(self):
+        print(f'Connexion depuis : {clientAddress}')
+        self.cSocket.send(
+            bytes("Le serveur te salue ...!\nÉcris 'bye' pour te déco", 'utf-8')
+        )
+        msg = ''
+        while True:
+            data = self.cSocket.recv(2048)
+            msg = data.decode()
+            if msg == 'bye':
+                break
+            print(f"From client {msg}")
+            self.cSocket.send(bytes(msg, 'utf-8'))
+        print(f"Client {clientAddress} disconnected...")
+
+
+# IP et port du serveur
+IP_SERVER = "127.0.0.1"
+PORT = 63000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((IP_SERVER, PORT))
+print("Server started")
+print("Waiting for client request..")
+while True:
+    server.listen(50)
+    clientSocket, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientSocket)
+    newthread.run()
+'''
+
+
+'''
 import socket
 import sys
 from _thread import *
@@ -41,6 +166,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
         list_of_clients.append(connection_client)
         print(f'{address_client[0]} connected')
         start_new_thread(client_thread, (connection_client, address_client))
+'''
 
 '''
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen_socket:
