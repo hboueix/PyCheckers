409c4fe9e21ddf7706b5bb39c5f933881ed65e08
Hugo Boueix <hboueix99@gmail.com>
2020-05-18 13:39:12 +0200

    :building_construction: cleaning draw function

--- app/modules/checkerboard.py
+++ app/modules/checkerboard.py
@@ -10,11 +10,8 @@ class Checkerboard(pygame.sprite.Sprite):
         self.squaresize = self.rect.w / 8
         self.squarecolor = {'white': [200, 200, 200], 'black': [100, 100, 100]}
         self.boxes = [[i for i in range(8)] for _ in range(8)]
-
-    def draw(self, screen):
         for x in range(1, 9):
             for y in range(1, 9):
-                box = self.boxes[x-1][y-1]
                 coords = [
                     self.rect.x + self.squaresize * (x - 1),
                     self.rect.y + self.squaresize * (y - 1)
@@ -24,21 +21,22 @@ class Checkerboard(pygame.sprite.Sprite):
                         box = Checkerbox(
                             self.squaresize, self.squarecolor['white'], coords
                         )
-                        box.draw(screen)
                     else:
                         box = Checkerbox(
                             self.squaresize, self.squarecolor['black'], coords
                         )
-                        box.draw(screen)
                 else:
                     if y % 2 != 0:
                         box = Checkerbox(
                             self.squaresize, self.squarecolor['black'], coords
                         )
-                        box.draw(screen)
                     else:
                         box = Checkerbox(
                             self.squaresize, self.squarecolor['white'], coords
                         )
-                        box.draw(screen)
                 self.boxes[x-1][y-1] = box
+
+    def draw(self, screen):
+        for line in self.boxes:
+            for box in line:
+                box.draw(screen)
